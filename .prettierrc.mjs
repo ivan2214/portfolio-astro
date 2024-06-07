6de484a9954383b568e9256f7a97b30535856337
Ivan Bongiovanni <bongiovanniivan12gmail.com>
2024-06-06 22:22:47 -0300

    init commit

---
+++ .prettierrc.mjs
@@ -0,0 +1,8 @@
+/** @type {import('prettier').Config} */
+export default {
+  printWidth: 80,
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  plugins: ['prettier-plugin-astro', 'prettier-plugin-tailwindcss'],
+}
