5474957508d47762b8c3cb087db0e8bbb9a28b0c
Ivan Bongiovanni <bongiovanniivan12gmail.com>
2024-07-18 22:18:41 -0300

    chore: change the output

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,17 @@
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel/serverless';
+import { defineConfig } from 'astro/config'
+import react from '@astrojs/react'
+import tailwind from '@astrojs/tailwind'
 
-import sitemap from "@astrojs/sitemap";
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ivanbong.vercel.app',
-  integrations: [react(), tailwind({
-    applyBaseStyles: false
-  }), sitemap()],
-  output: 'server',
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true
-    },
-    imageService: false
-  })
-});
+  integrations: [
+    react(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    sitemap(),
+  ],
+})
